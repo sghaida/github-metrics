6eadf6a90f8b240847930e952ac411c022bf2c0c
sghaida <saddam.abughaida@gmail.com>
2023-06-12 13:25:03 +0200

    add frontend metrics

--- src/prs_process.go
+++ src/prs_process.go
@@ -31,42 +31,44 @@ func NewPRProcessor(client *github.Client, conf *Config, send chan<- RepoPrs) *P
 	}
 }
 
+func (p *PRProcessor) process(owner, repo string, counter *int32, from, to time.Time) {
+	var enrichedPrs []PRInfo
+	ctx := context.Background()
+
+	prs, err := GetRepoPrs(
+		ctx, from, to, p.config.Org, repo, p.client,
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, pr := range prs {
+		comments, err := GetPrComments(ctx, p.config.Org, repo, pr.OwnerName, pr.PrNumber, p.client)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		pr.CommentInfo = comments
+		enrichedPrs = append(enrichedPrs, pr)
+	}
+
+	p.ch <- RepoPrs{
+		Repo: repo,
+		Prs:  enrichedPrs,
+	}
+	atomic.AddInt32(counter, 1)
+}
+
 func (p *PRProcessor) GetPrs(from, to time.Time) {
 	var counter int32
 	atomic.StoreInt32(&counter, 0)
 
 	for _, repo := range p.config.Repos.Backend {
-
-		go func(owner, repo string) {
-
-			var enrichedPrs []PRInfo
-			ctx := context.Background()
-
-			prs, err := GetRepoPrs(
-				ctx, from, to, p.config.Org, repo, p.client,
-			)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			for _, pr := range prs {
-				comments, err := GetPrComments(ctx, p.config.Org, repo, pr.OwnerName, pr.PrNumber, p.client)
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				pr.CommentInfo = comments
-				enrichedPrs = append(enrichedPrs, pr)
-			}
-
-			p.ch <- RepoPrs{
-				Repo: repo,
-				Prs:  enrichedPrs,
-			}
-			atomic.AddInt32(&counter, 1)
-
-		}(config.Org, repo)
+		go p.process(config.Org, repo, &counter, from, to)
+	}
+	for _, repo := range p.config.Repos.Frontend {
+		go p.process(config.Org, repo, &counter, from, to)
 	}
 
 	p.wg.Add(1)
@@ -74,7 +76,7 @@ func (p *PRProcessor) GetPrs(from, to time.Time) {
 		defer p.wg.Done()
 		for {
 			c := atomic.LoadInt32(&counter)
-			if c == int32(len(config.Repos.Backend)) {
+			if c == int32(len(config.Repos.Backend)+len(config.Repos.Frontend)) {
 				close(p.ch)
 				break
 			}
